1d93070ce26aa0d5835bc088a8206e07d0164345
Lucas Gonçalves <lucas.g.apostolo@gmail.com>
2024-09-22 02:37:53 -0300

    Feat: allow cors and add port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,18 @@
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import fastify from "fastify";
+import fastifyCors from '@fastify/cors'
+import {createCulture} from './router/createCulture'
+import {getCulture} from './router/getCulture'
 
-export const app = fastify()
+export const app = fastify().withTypeProvider<TypeBoxTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: '*'
+})
+
+app.register(createCulture)
+app.register(getCulture)
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log("HTTP Serber running!")
+})
